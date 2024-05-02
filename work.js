2bf891e76b889da441499fec7a6b20b5cb1609dc
Your Name <you@example.com>
2024-05-02 06:23:22 +0300

    first commit

---
+++ work.js
@@ -0,0 +1,51 @@
+// task1
+
+// function submit(){
+//     let username = document.getElementById("username").value
+//     let password = +document.getElementById("password").value
+//     if(username == "admin"){
+//         if(password == 1234){
+//             alert("Success")
+//         }else{
+//             alert("password ERROR")
+//         }
+//     }else if(password == 1234){
+//         alert("Username ERROR")
+//     }else{
+//         alert("Username and password Error")
+//     }
+// }
+// function isShow(){
+//     let password = document.getElementById("password")
+//     if(password.type === "password"){
+//         password.type = "text"
+//     }else{
+//         password.type = "password"
+//     }
+// }
+
+
+// task2
+
+// const  create = (e) => {
+//     e.preventDefault();
+//     let result = document.getElementById("result")
+//     let width = document.getElementById("width").value
+//     let hight = document.getElementById("hight").value
+//     let bg_color = document.getElementById("b-radius").value
+//     let b_radius = document.getElementById("color").value
+//     result.style.width = `${width}px`
+//     result.style.height = `${hight}`
+//     result.style.backgroundColor = bg_color
+//     result.style.borderRadius = `${b_radius}px`
+// }
+
+
+// task3
+
+// function str_count(item){
+//     item.preventDefault();
+//     let str = document.getElementById("str").value
+//     let arr = str.split(" ")
+//     console.log(arr.length);
+// }
